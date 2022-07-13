6267cd655da56a4fe4d5b9fdf2bad27bfa5e98e0
Darion <darionjwilliams@gmail.com>
2022-07-12 23:23:23 -0700

    Initial init. Bot runs, commands deploy

---
+++ src/commands/deploy.ts
@@ -0,0 +1,16 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { REST } from '@discordjs/rest';
+import { Routes } from 'discord-api-types/v9';
+import { clientId, guildId, token } from '../../config.json';
+
+const commands = [
+	new SlashCommandBuilder().setName('test').setDescription('Am I working?')
+]
+	.map(command => command.toJSON());
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
+
